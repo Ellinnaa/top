9401e0041026c614f261cf437084d8443249d7bb
Ellinna <you@example.com>
2025-03-31 15:43:18 +0200

    commit all changes

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-var map = L.map('map').setView([50.914167, 14.203333], 15);
+/*var map = L.map('map').setView([50.914167, 14.203333], 15);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 var marker = L.marker([50.914167, 14.203333]).addTo(map);
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-
+*/
 
 let stop= {
     nr: 2,
@@ -17,7 +17,7 @@ let stop= {
 };
 
 const STOPS= [
-    
+
 ];
 console.log(STOPS[0]);
 console.log(STOPS[0].title);
